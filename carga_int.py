aab238b88d5f17cabd9ea7bf154d533bfb5266fd
Marcelo Alejandro Graef <m.graef@ptytechnologies.com>
2021-10-06 00:13:13 -0300

    daterange

--- carga_int.py
+++ carga_int.py
@@ -1,5 +1,5 @@
 """
-    @author Juan Pablo Mantelli 
+    @author Juan Pablo Mantelli
 """
 import json
 import requests
@@ -20,14 +20,10 @@ if __name__ == "__main__":
     print()
 
     id_lote = functions.getLoteKey('Carga_Int_Fact_Py')
-    print()
-    print()
-    print("idLote: ", id_lote)
-    print()
-    print()
+
     response_process = functions.getLastProcessKeyByLote(id_lote)
     functions.updateProcessState(response_process.Proceso_Key, 1)
-    
+
     fechas = [
         response_process.fecha_desde_proceso,
         response_process.fecha_hasta_proceso
@@ -36,93 +32,101 @@ if __name__ == "__main__":
     print(fechas[0].strftime('%Y-%m-%d'))
     print(fechas[1].strftime('%Y-%m-%d'))
 
-    ## Está aparte por que se envie una vez con dol y otra por pes, además del parametro tipo moneda en 1
+    print()
+    print("get_date_range()")
+    print(get_date_range())
+
+    # Está aparte por que se envie una vez con dol y otra por pes, además del parametro tipo moneda en 1
     conDobleMoneda = [
-        ['AnlisisisDeSanidad',"Int_Sanidad"], 
-        ['suplementacionHaciendaELN',"Int_Suplementacion"], 
-        ['analisisLiquidacionesGranos',"Int_Liq_Granos"], 
-        ['IngresosYEgresosDeHacienda',"Int_Hacienda_Ingresos_Egresos"], 
-        ['analisisLiquidacionesHacienda',"Int_Liquidaciones_Hacienda"] 
+        ['AnlisisisDeSanidad', "Int_Sanidad"],
+        ['suplementacionHaciendaELN', "Int_Suplementacion"],
+        ['analisisLiquidacionesGranos', "Int_Liq_Granos"],
+        ['IngresosYEgresosDeHacienda', "Int_Hacienda_Ingresos_Egresos"],
+        ['analisisLiquidacionesHacienda', "Int_Liquidaciones_Hacienda"]
     ]
 
-    ingresosYEgresos = [ 
-        ['ingresosYEgresos',"Int_Ingresos_Egresos_Deposito"]
+    ingresosYEgresos = [
+        ['ingresosYEgresos', "Int_Ingresos_Egresos_Deposito"]
     ]
 
     consumosProduccion = [
-       ['analisisConsumosProduccion',"Int_Analisis_Consumo_Produccion"] 
+        ['analisisConsumosProduccion', "Int_Analisis_Consumo_Produccion"]
     ]
 
-
     planificaciones = [
-        ['planificacionAgricola',"Int_Planificacion_Agricola"],
-        ['planificacionForestal',"Int_Planificacion_Forestal"]
+        ['planificacionAgricola', "Int_Planificacion_Agricola"],
+        ['planificacionForestal', "Int_Planificacion_Forestal"]
     ]
 
     mainstables2 = [
-        ['AnalisisHaciendaEventoYClasificacion',"Int_Hacienda_Evento_Clasificacion"]
+        ['AnalisisHaciendaEventoYClasificacion',
+            "Int_Hacienda_Evento_Clasificacion"]
     ]
 
     analisislote = [
-        ['AnalisisDeLote',"Int_Analisis_Lote"] ## PARAMWEBREPORT_VerMonedaEn a este se le envia un 1 o un 2.. es distinto al resto
+        # PARAMWEBREPORT_VerMonedaEn a este se le envia un 1 o un 2.. es distinto al resto
+        ['AnalisisDeLote', "Int_Analisis_Lote"]
     ]
 
     stock = [
-       ['existenciaHaciendaELN',"int_StockXLote"], 
-       ['existenciaHaciendaELN',"int_StockXLoteTropa"] 
+        ['existenciaHaciendaELN', "int_StockXLote"],
+        ['existenciaHaciendaELN', "int_StockXLoteTropa"]
     ]
- 
+
     libromayoreln = [
-        ['LIBROMAYORELN',"Int_LIBROMAYORELN"] 
+        ['LIBROMAYORELN', "Int_LIBROMAYORELN"]
     ]
- 
+
     libromayorelnx = [
-        ['LIBROMAYORELNACTIVO',"int_Mayor_Activo"],
-        ['LIBROMAYORELNPASIVO',"Int_Mayor_Pasivo"],
-        ['LIBROMAYORELNPN',"Int_Mayor_Pn"]
+        ['LIBROMAYORELNACTIVO', "int_Mayor_Activo"],
+        ['LIBROMAYORELNPASIVO', "Int_Mayor_Pasivo"],
+        ['LIBROMAYORELNPN', "Int_Mayor_Pn"]
     ]
 
     response = api_requests.init()
-        
+
     token = response.text
 
     for table in consumosProduccion:
 
-        monedas = ["PES","DOL"]
-        dimension = ["DIMCTC","DIMMAQ"]
-        
-        for x in range(0,len(monedas)):
-            for y in range(0,len(dimension)):
-                
-                response = api_requests.int_conMoneda(token, monedas[x], table[0], fechas, dimension[y])
+        monedas = ["PES", "DOL"]
+        dimension = ["DIMCTC", "DIMMAQ"]
+
+        for x in range(0, len(monedas)):
+            for y in range(0, len(dimension)):
+
+                response = api_requests.int_conMoneda(
+                    token, monedas[x], table[0], fechas, dimension[y])
 
                 myObj = response.json()
                 for field_dict in myObj:
                     field_dict['TIPO_DIMENSION'] = dimension[y]
-                    
-                #functions.deleteTable(table[1])
+
+                # functions.deleteTable(table[1])
                 functions.insertDimension(pd.DataFrame(myObj), table[1])
                 # functions.auditoria(table[1], response_process.Proceso_Key)
     conexion.commit()
     for table in conDobleMoneda:
 
-        monedas = ["PES","DOL"]
-        dimension = ["DIMCTC","DIMMAQ"] # Agregue la dimesion moneda por que esta hardcodead un solo tipo DIMCTC y dim DIMMAQ
-        for x in range(0,len(monedas)):
-            for y in range(0,len(dimension)):
+        monedas = ["PES", "DOL"]
+        # Agregue la dimesion moneda por que esta hardcodead un solo tipo DIMCTC y dim DIMMAQ
+        dimension = ["DIMCTC", "DIMMAQ"]
+        for x in range(0, len(monedas)):
+            for y in range(0, len(dimension)):
 
-                response = api_requests.int_conMoneda(token, monedas[x], table[0], fechas, dimension[y]) # remplaze 'DIMCTC' por dimension
+                response = api_requests.int_conMoneda(
+                    token, monedas[x], table[0], fechas, dimension[y])  # remplaze 'DIMCTC' por dimension
                 print(response)
                 myObj = response.json()
-                
-                #functions.deleteTable(table[1])
+
+                # functions.deleteTable(table[1])
                 functions.insertDimension(pd.DataFrame(myObj), table[1])
                 # functions.auditoria(table[1], response_process.Proceso_Key)
     conexion.commit()
-    
+
     for st in stock:
 
-        monedas = ["PES","DOL"]
+        monedas = ["PES", "DOL"]
         agrupa_por = -1
 
         if st[1] == 'int_StockXLote':
@@ -131,67 +135,68 @@ if __name__ == "__main__":
         if st[1] == 'int_StockXLoteTropa':
             agrupa_por = 0
 
-        
-        for x in range(0,len(monedas)):
+        for x in range(0, len(monedas)):
 
-            response = api_requests.int_stock(token, monedas[x], st[0],fechas, agrupa_por)
+            response = api_requests.int_stock(
+                token, monedas[x], st[0], fechas, agrupa_por)
 
             myObj = response.json()
-            
-            #functions.deleteTable(st[1])
+
+            # functions.deleteTable(st[1])
             functions.insertDimension(pd.DataFrame(myObj), st[1])
             # functions.auditoria(st[1], response_process.Proceso_Key)
-    conexion.commit()        
+    conexion.commit()
     for lme in libromayorelnx:
 
-        monedas = ["PES","DOL"]
-        
-        for x in range(0,len(monedas)):
-           
+        monedas = ["PES", "DOL"]
+
+        for x in range(0, len(monedas)):
+
             response = api_requests.init()
-    
+
             token = response.text
             print(datetime.now())
-            response = api_requests.libromayorelnx(token, monedas[x], fechas, lme[0])
+            response = api_requests.libromayorelnx(
+                token, monedas[x], fechas, lme[0])
             print(datetime.now())
             print(response)
             myObj = response.json()
-            
-            #functions.deleteTable(lme[1])
+
+            # functions.deleteTable(lme[1])
             functions.insertDimension(pd.DataFrame(myObj), lme[1])
             # functions.auditoria(lme[1], response_process.Proceso_Key)
     conexion.commit()
 
     for lme in libromayoreln:
 
-        monedas = ["PES","DOL"]
-        dimension = ["DIMCTC","DIMMAQ"]
-        
-        for x in range(0,len(monedas)):
-            for y in range(0,len(dimension)):
+        monedas = ["PES", "DOL"]
+        dimension = ["DIMCTC", "DIMMAQ"]
 
+        for x in range(0, len(monedas)):
+            for y in range(0, len(dimension)):
 
-                response = api_requests.libromayoreln(token, monedas[x], fechas,  dimension[y])
+                response = api_requests.libromayoreln(
+                    token, monedas[x], fechas,  dimension[y])
                 print(response)
                 myObj = response.json()
                 for field_dict in myObj:
                     field_dict['TIPO_DIMENSION'] = dimension[y]
-                    
-                    #functions.deleteTable(lme[1])
+
+                    # functions.deleteTable(lme[1])
                     functions.insertDimension(pd.DataFrame(myObj), lme[1])
                     # functions.auditoria(lme[1], response_process.Proceso_Key)
-    conexion.commit()    
+    conexion.commit()
     for table2 in mainstables2:
 
         response = api_requests.interface(token, table2[0])
 
         myObj = response.json()
 
-        #functions.deleteTable(table2[1])
+        # functions.deleteTable(table2[1])
         functions.insertDimension(pd.DataFrame(myObj), table2[1])
         # functions.auditoria(table2[1], response_process.Proceso_Key)
-        
-    conexion.commit()    
+
+    conexion.commit()
     for planif in planificaciones:
 
         response = api_requests.planificaciones(token, planif[0])
@@ -200,45 +205,41 @@ if __name__ == "__main__":
 
         myObj = response.json()
 
-        #functions.deleteTable(planif[1])
+        # functions.deleteTable(planif[1])
         functions.insertDimension(pd.DataFrame(myObj), planif[1])
         # functions.auditoria(planif[1], response_process.Proceso_Key)
     conexion.commit()
     for lote in analisislote:
 
-        monedas = [0,1]
-        
-        for x in range(0,len(monedas)):
+        monedas = [0, 1]
 
+        for x in range(0, len(monedas)):
 
             response = api_requests.LoteAna(token, monedas[x], lote[0], fechas)
             print(response)
             myObj = response.json()
-            
-            #functions.deleteTable(lote[1])
+
+            # functions.deleteTable(lote[1])
             functions.insertDimension(pd.DataFrame(myObj), lote[1])
             # functions.auditoria(lote[1], response_process.Proceso_Key)
 
     conexion.commit()
     for table in ingresosYEgresos:
 
-        monedas = ["PES","DOL"]
-        
-        for x in range(0,len(monedas)):
+        monedas = ["PES", "DOL"]
 
+        for x in range(0, len(monedas)):
 
-            response = api_requests.int_ingresosYEgresos(token, monedas[x], table[0], fechas)
+            response = api_requests.int_ingresosYEgresos(
+                token, monedas[x], table[0], fechas)
             print(response)
             myObj = response.json()
-            
-            #functions.deleteTable(table[1])
+
+            # functions.deleteTable(table[1])
             functions.insertDimension(pd.DataFrame(myObj), table[1])
             # functions.auditoria(table[1], response_process.Proceso_Key)
-    
-    #close the connection to the database.
+
+    # close the connection to the database.
     conexion.commit()
     functions.updateProcessState(response_process.Proceso_Key, 2)
     cursor.close()
-
-
-
--- functions.py
+++ functions.py
@@ -19,6 +19,7 @@ def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
+
 def process_data(value):
     if type(value) in (str, bytes):
         value = value.replace("None", "null")
@@ -32,14 +33,15 @@ def process_data(value):
         value = str(value).replace("'", "")
     return value
 
+
 def insertDimension(data, TABLE_NAME):
 
     start_time = time.time()
-    
+
     print(f'{"-"*50}')
     print(f'{TABLE_NAME}')
-    #print(datetime.datetime.now())
-  
+    # print(datetime.datetime.now())
+
     columns_fixed = []
     for col in data.columns:
         col = col.replace("-", "")
@@ -59,48 +61,69 @@ def insertDimension(data, TABLE_NAME):
     for col in data.columns:
         data[col] = data[col].apply(process_data)
 
-
-    #print('Comenzando la carga de la tabla',TABLE_NAME)   
+    #print('Comenzando la carga de la tabla',TABLE_NAME)
     data.to_sql(TABLE_NAME, con=get_engine(), index=False, if_exists='replace')
-
 
     print(f'{round(time.time() - start_time, 2)}seg')
     print()
     print()
 
 
-
 def deleteTable(table):
     with get_cursor() as cursor:
-        cursor.execute("DELETE FROM "+ table )
+        cursor.execute("DELETE FROM " + table)
+
 
 def getLoteKey(descripcion):
     with get_cursor() as cursor:
-        cursor.execute("SELECT Lote_Key FROM Esq_Proc_Dw_Lotes WHERE Descripcion = '"+ descripcion +"'")
+        cursor.execute(
+            "SELECT Lote_Key FROM Esq_Proc_Dw_Lotes WHERE Descripcion = '" + descripcion + "'")
         result = cursor.fetchone()
         for r in result:
             return r
 
+
 def getLastProcessKeyByLote(idLote):
     with get_cursor() as cursor:
-        cursor.execute("SELECT MAX([Proceso_Key]) as Proceso_Key, fecha_desde_proceso, fecha_hasta_proceso FROM Esq_Proc_Dw_Procesos WHERE Lote_Key = (?) AND [Estado_Key] = 0 group by fecha_desde_proceso, fecha_hasta_proceso", (idLote))
+        cursor.execute(
+            "SELECT MAX([Proceso_Key]) as Proceso_Key, fecha_desde_proceso, fecha_hasta_proceso FROM Esq_Proc_Dw_Procesos WHERE Lote_Key = (?) AND [Estado_Key] = 0 group by fecha_desde_proceso, fecha_hasta_proceso", (idLote))
         return cursor.fetchone()
+
 
 def insertProcessKey(id_lote, fecha_inicio, fecha_fin):
     with get_cursor() as cursor:
-        cursor.execute("INSERT INTO [dbo].[Esq_Proc_Dw_Procesos] ([Fecha_Inicio_Ejecucion],[Fecha_Desde_Proceso],[Fecha_Hasta_Proceso],[Estado_Key],[Lote_Key],[IdControl], IdUsuarioEjecucion) VALUES((?),'"+ fecha_inicio +"','"+ fecha_fin +"',0,(?),0,3)", (datetime.datetime.now(),id_lote))
+        cursor.execute("INSERT INTO [dbo].[Esq_Proc_Dw_Procesos] ([Fecha_Inicio_Ejecucion],[Fecha_Desde_Proceso],[Fecha_Hasta_Proceso],[Estado_Key],[Lote_Key],[IdControl], IdUsuarioEjecucion) VALUES((?),'" +
+                       fecha_inicio + "','" + fecha_fin + "',0,(?),0,3)", (datetime.datetime.now(), id_lote))
         cursor.commit()
+
 
 def updateProcessState(process_key, state):
     with get_cursor() as cursor:
-        cursor.execute("UPDATE [dbo].[Esq_Proc_Dw_Procesos] SET [Estado_Key] = (?)  WHERE [Proceso_Key] = (?)", (state, process_key))
+        cursor.execute(
+            "UPDATE [dbo].[Esq_Proc_Dw_Procesos] SET [Estado_Key] = (?)  WHERE [Proceso_Key] = (?)", (state, process_key))
         cursor.commit()
+
 
 def updateTable(table, field, value):
     with get_cursor() as cursor:
-        cursor.execute("UPDATE "+ table +" SET "+ field +" = (?) WHERE "+ field +" IS NULL", (value))
+        cursor.execute("UPDATE " + table + " SET " + field +
+                       " = (?) WHERE " + field + " IS NULL", (value))
 
 
 def auditoria(table, process_key):
     with get_cursor() as cursor:
-        cursor.execute("UPDATE "+ table +" SET fecha_proce_escritura = (?), Proceso_Key = (?)", (datetime.datetime.now(),process_key))
+        cursor.execute("UPDATE " + table + " SET fecha_proce_escritura = (?), Proceso_Key = (?)",
+                       (datetime.datetime.now(), process_key))
+
+
+def last_day_of_month(any_day):
+    # this will never fail
+    # get close to the end of the month for any day, and add 4 days 'over'
+    next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
+    # subtract the number of remaining 'overage' days to get last day of current month, or said programattically said, the previous day of the first of next month
+    return next_month - datetime.timedelta(days=next_month.day)
+
+
+def get_date_range():
+    today = datetime.datetime.today()
+    return [today.replace(day=1), last_day_of_month(today)]
