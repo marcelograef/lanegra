843f1ac35ce405e02bdf626d10bd67a94b9a1570
Marcelo Alejandro Graef <m.graef@ptytechnologies.com>
2021-11-05 00:46:40 -0300

    fix names int

--- carga_fact.py
+++ carga_fact.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
     ]
 
     libromayoreln = [
-        ['LIBROMAYORELN', "Int_LIBROMAYORELN"]
+        ['LIBROMAYORELN', "Int_Libromayoreln"]
     ]
 
     libromayorelnx = [
